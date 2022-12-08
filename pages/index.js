4ac894c0432ec94caa718d0a0e2c63cd0675e242
Daniel Rejment <daniel@rejment.com>
2022-12-08 19:02:40 +0100

    Update index.js
    Editing in github is hard :)

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to the Tacton AoC Web!</a>
+          Welcome to the Tacton AoC Web!
         </h1>
       </main>
 
