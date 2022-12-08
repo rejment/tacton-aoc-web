811c0789997975d6cacf857f8ede2bec31a48243
Daniel Rejment <daniel@rejment.com>
2022-12-08 21:01:24 +0100

    Basic operation, listing the tacton leaderboard.

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,34 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import { getLeaderboard } from '../lib/leaderboard'
 
-export default function Home() {
+export default function Home({leaderboard}) {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Tacton AoC Web</title>
         <meta name="description" content="Tacton AoC Web" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to the Tacton AoC Web!
-        </h1>
+      <main>
+        <h1>Welcome to the Tacton AoC Web!</h1>
+        Members:
+        <ul>
+          {Object.values(leaderboard.members)
+            .filter(member => member.local_score>0)
+            .sort((a,b)=>b.local_score-a.local_score)
+            .map(member => (
+            <li key={member.id}>{member.name} {member.local_score}p</li>
+          ))}
+        </ul>
+
       </main>
 
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const leaderboard = await getLeaderboard()
+  return { props: { leaderboard } }
 }
