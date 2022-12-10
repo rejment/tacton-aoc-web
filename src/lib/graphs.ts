53bacc8c7d01a0f045b14b1c3537cbaa9dcfd6a1
Daniel Rejment <daniel.rejment@tacton.com>
2022-12-10 07:20:06 +0100

    Fix linter error

--- src/lib/graphs.ts
+++ src/lib/graphs.ts
@@ -2,7 +2,7 @@ import type { LeaderBoard } from "../types/leaderboard";
 import type { Graph, GraphMaker } from "../types/graph";
 import { ScoreGraph } from "./graphs/scoregraph";
 
-export function getAllGraphs(leaderboard : LeaderBoard) : Report[] {
+export function getAllGraphs(leaderboard : LeaderBoard) : Graph[] {
     return graphs.map(r => r(leaderboard));
 }
 
