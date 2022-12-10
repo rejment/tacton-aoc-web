6203cf8783ed18558166695f99f291b2a6e52e06
Daniel Rejment <daniel.rejment@tacton.com>
2022-12-10 07:35:37 +0100

    remove the dependency on canvas

--- src/lib/graphs.ts
+++ src/lib/graphs.ts
@@ -1,12 +1,11 @@
 import type { LeaderBoard } from "../types/leaderboard";
 import type { Graph, GraphMaker } from "../types/graph";
-import { ScoreGraph } from "./graphs/scoregraph";
+
 
 export function getAllGraphs(leaderboard : LeaderBoard) : Graph[] {
     return graphs.map(r => r(leaderboard));
 }
 
 const graphs : GraphMaker[] = [
-    ScoreGraph
 ];
 
