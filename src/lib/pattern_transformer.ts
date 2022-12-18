c0b1cfccb9647a32c0b0c996ef867e8b1686edbe
Jean-Noël Monette <janhonho@gmail.com>
2022-12-18 18:32:24 +0100

    Improvements to the ascii art obfuscation (#11)
    * Adding Max Time Solving Report
    
    * Extra column for solved all
    
    * Add some ascii art that is progressively revealed as we collect stars over the days.
    
    * Update to the ASCII art: new image, put it first, only for 2022, just add up all collected stars
    
    * Fix file reading in vercel?
    
    * Improvements to the ascii art obfuscation
    
    Co-authored-by: Jean-Noël Monette <jean-noel.monette@tacton.com>

--- src/lib/pattern_transformer.ts
+++ src/lib/pattern_transformer.ts
@@ -3,9 +3,7 @@ import {range} from "./range";
 
 export function getTransformedPattern(leaderboard: LeaderBoard, pattern: String) {
 
-    const starCounts = range(1, 50).map(i => 0);
-
-    let totalStars = Object.values(leaderboard.members).map(member => member.stars).reduce((a,b) => a + b, 0);
+    let totalStars = Object.values(leaderboard.members).map(member => member.stars).reduce((a, b) => a + b, 0);
 
     const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRTSTUVWXYZ";
 
@@ -13,10 +11,31 @@ export function getTransformedPattern(leaderboard: LeaderBoard, pattern: String)
         return chars.at((a * 31 + b * 17 + c * 9) % 57 % chars.length);
     }
 
-    //Currently, the art is revealed row per row.
-    //TODO: Find a way to reveal the art in a nicer, like from the middle first, or from the edges first.
-    return Array.from(pattern)
-        .map((char, index) =>
-            (char==="\n" || totalStars > index ? char : pseudoRandChar(index, totalStars))).join("").replaceAll(" ", "\u00A0");
+    const lines = pattern.split("\n");
+    const mid = [lines.length / 2, lines.map(l => l.length).reduce((a, b) => Math.max(a, b), 0) / 2];
 
+    function dist([x0, y0], [x1, y1]) {
+        //return 3*Math.abs(x0-x1) + Math.abs(y0-y1); //This gives a diamond/losange shape
+        return Math.max(3 * Math.abs(x0 - x1), Math.abs(y0 - y1)); //This gives a rectangle shape
+        //return Math.sqrt(5* (x0 - x1) * (x0 - x1) + (y0 - y1) * (y0 - y1)); //This gives a round/oval shape
+    }
+
+    const coordinates = lines.flatMap((line, i) => Array.from(line).map((c, j) => [i, j]));
+    coordinates.sort((a, b) => dist(a, mid) - dist(b, mid));
+    coordinates.length = Math.min(coordinates.length, totalStars);
+    const revealed = lines.map(line => Array.from(line).map(c => false));
+    for (let [x, y] of coordinates) {
+        revealed[x][y] = true;
+    }
+
+    const remaining = lines.map(l => l.length).reduce((a, b) => a + b, 0) - totalStars;
+
+    return {
+        pattern: lines.map((line, i) =>
+            Array.from(line).map((c, j) =>
+                revealed[i][j] ? c : pseudoRandChar(i, j, totalStars)
+            ).join("").replaceAll(" ", "\u00A0")
+        ).join("\n"),
+        remaining
+    };
 }
--- src/pages/[year]/index.tsx
+++ src/pages/[year]/index.tsx
@@ -4,7 +4,6 @@ import { getAllReports } from '../../lib/reports'
 import { getAllGraphs } from '../../lib/graphs'
 import { getTransformedPattern } from "../../lib/pattern_transformer";
 
-import Image from 'next/image'
 import { range } from '../../lib/range';
 import { readFile } from "fs/promises";
 import path from "path";
@@ -12,53 +11,70 @@ import path from "path";
 export default function Page({leaderboard, year, pattern}) {
   const reports = getAllReports(leaderboard);
   const graphs = getAllGraphs(leaderboard);
+  const transformed = getTransformedPattern(leaderboard, pattern)
 
   return (
-    <PageWrapper title="Tacton AoC Web">
+      <PageWrapper title="Tacton AoC Web">
 
-        <h1>Welcome to the {year} Tacton AoC Web!</h1>
+          <h1>Welcome to the {year} Tacton AoC Web!</h1>
 
-        <ul id="years">
-            {range(2015,2022).map(y=>(
-                <li className={year==y?'active':''} key={y}><a href={`/${y}`}>{y}</a></li>
-            ))}
-        </ul>
+          <ul id="years">
+              {range(2015, 2022).map(y => (
+                  <li className={year == y ? 'active' : ''} key={y}><a href={`/${y}`}>{y}</a></li>
+              ))}
+          </ul>
 
-        <div key="ascii-art">
-            <pre>
-                {getTransformedPattern(leaderboard, pattern)}
-            </pre>
-        </div>
+          {
+              transformed.pattern.length === 0 ?
+                  <div/>
+                  :
+                  <div key="ascii-art">
+                      <h3>Overall Progress</h3>
+                      <pre>{transformed.pattern}</pre>
+                      <div>
+                          One character is revealed for each star obtained by anyone on the leaderboard.
+                          {
+                              (transformed.remaining > 0) ?
+                                  <div>Let's collect as many of them to reveal the entire image!
+                                      Only {transformed.remaining} left to reach the goal...</div>
+                                  :
+                                  <div>Enough stars have been collected. Well done</div>
+                          }
+                      </div>
 
-        { reports.map(report => (
-            <div key={report.title}>
-              {report.title}
-              <table>
-              <thead>
-                  <tr>
-                    {report.columns.map(col => (<th key={col}>{col}</th>))}
-                  </tr>
-                </thead>
-                <tbody>
-                  {report.lines.map(line=>(
-                    <tr key={line.join("-")}>
-                    {line.map((cell,i) => (<td key={cell+i}>{cell}</td>))}
-                  </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-        ))}
-        { graphs.map(graph => (
-            <div key={graph.title}>
-              <>
-                {graph.title}
-                <Canvas width={700} height={400} style={{width:"700px"}} render={canvas=>graph.render(canvas, leaderboard)}/>
-              </>
-            </div>
-        ))}
+                  </div>
+          }
 
-    </PageWrapper>
+          {reports.map(report => (
+              <div key={report.title}>
+                  <h3>{report.title}</h3>
+                  <table>
+                      <thead>
+                      <tr>
+                          {report.columns.map(col => (<th key={col}>{col}</th>))}
+                      </tr>
+                      </thead>
+                      <tbody>
+                      {report.lines.map(line => (
+                          <tr key={line.join("-")}>
+                              {line.map((cell, i) => (<td key={cell + i}>{cell}</td>))}
+                          </tr>
+                      ))}
+                      </tbody>
+                  </table>
+              </div>
+          ))}
+          {graphs.map(graph => (
+              <div key={graph.title}>
+                  <>
+                      <h3>{graph.title}</h3>
+                      <Canvas width={700} height={400} style={{width: "700px"}}
+                              render={canvas => graph.render(canvas, leaderboard)}/>
+                  </>
+              </div>
+          ))}
+
+      </PageWrapper>
   )
 }
 
